be21fc3fd0bc714bf7d27f4ded4e8b2c0b4833e9
upupnoah <upupqi.cs@gmail.com>
2024-08-11 22:55:01 +0700

    feat(darling): support darling

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 mod enum_from;
+mod enum_from_darling;
 
 use enum_from::process_enum_from;
+use enum_from_darling::process_enum_from_darling;
 use proc_macro::TokenStream;
 
 /// for enum, we'd like to generate From impls for each variant
@@ -10,4 +12,12 @@ pub fn derive_enum_from(input: TokenStream) -> TokenStream {
     println!("{:#?}", input);
 
     process_enum_from(input).into()
+}
+
+#[proc_macro_derive(EnumFromDarling)]
+pub fn derive_enum_from_darling(input: TokenStream) -> TokenStream {
+    let input = syn::parse_macro_input!(input as syn::DeriveInput);
+    // println!("{:#?}", input);
+
+    process_enum_from_darling(input).into()
 }
